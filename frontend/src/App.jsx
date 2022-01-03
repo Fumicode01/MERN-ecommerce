7f889e2b4488416036c4f6725216aca433378b11
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-01-03 22:25:40 +1100

    Added Single product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 import ProductList from './pages/ProductList';
 
 function App() {
   return (
         // <Home />
-        <ProductList />
+        // <ProductList />
+        <ProductDetail />
   );
 }
 
