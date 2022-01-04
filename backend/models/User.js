e0d7b352939523cf90ba52c9f28637b02efe806a
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-01-04 16:34:33 +1100

    Added User CURD operations

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema(
     {
+        isAdmin:{type: Boolean, default: false},
         username:{type: String, required:true, unique:true},
         email:{type: String, required:true, unique:true},
         password:{type: String, required:true, unique:true},
-        isAdmin: {
-            type:Boolean,
-            defalut: false,
-        },
     },
     {timestamps: true}
 )
