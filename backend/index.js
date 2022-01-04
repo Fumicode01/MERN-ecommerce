b1d9faed03accac4df55c220569d8f1a13e5e9d3
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-01-04 13:22:59 +1100

    Added MongoDB authentication for register and login

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json())
 app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(process.env.PORT || 5000, (req, res) => {
     console.log("backend server is runnning")
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,41 @@
+const User = require("../models/User")
 const router = require("express").Router()
+const CryptoJS = require("crypto-js")
 
 
+//Register
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECREKEY).toString(),
+    });
+
+    try{
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Login
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(401).json("Wrong credentials")
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECREKEY);
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        const {password, ...others} = user._doc
+
+        originalPassword !== req.body.password && es.status(401).json("Wrong credentials")
+
+        res.status(200).json(others)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
