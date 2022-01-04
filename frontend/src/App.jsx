151dc82d31fb3c7deab4f9cf9a2db3fbdd923512
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-01-04 20:58:45 +1100

    Added Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
 import ProductList from './pages/ProductList';
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+
+
 
 function App() {
   return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products/:category" element={<ProductList />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                {/* {user ? <Route path="/" element={<Home />} /> : <Route path="/login" element={<Login />} />} */}
+            </Routes>
+        </Router>
         // <Home />
         // <ProductList />
-        <ProductDetail />
+        // <ProductDetail />
   );
 }
 
